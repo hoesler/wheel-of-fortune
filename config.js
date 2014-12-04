27b0bb526c5064b298630c92e26df3c6822c90e1
Christoph Hösler <christoph.hoesler@gmail.com>
2014-12-04 14:47:13 +0100

    Removed wheel

--- config.js
+++ config.js
@@ -1,15 +1,11 @@
 require.config({
   shim: {
-    palette: {
-      exports: 'palette'
-    }
   },
   paths: {
     require: "bower_components/requirejs/require",
     jquery: "bower_components/jquery/dist/jquery",
     chance: "bower_components/chance/chance",
     moment: "bower_components/momentjs/moment",
-    palette: "scripts/palette"
   },
   packages: [
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-require(["jquery", "chance", "moment", "palette"], function(jquery, chance, moment, palette) {
+require(["jquery", "chance", "moment"], function(jquery, chance, moment) {
 	spreadsheet_url = "https://spreadsheets.google.com/feeds/list/***REMOVED***/1/public/values?alt=json&amp;callback=importGSS";
 	
 	$.getJSON(spreadsheet_url, function( data ) {
@@ -25,41 +25,5 @@ require(["jquery", "chance", "moment", "palette"], function(jquery, chance, mome
 				break
 			}
 		}
-
-		function getTotal(myData){
-			var myTotal = 0;
-			for (var j = 0; j < myData.length; j++) {
-				myTotal += (typeof myData[j] == 'number') ? myData[j] : 0;
-			}
-			return myTotal;
-		}
-
-		function plotData(myData, myColor, canvas) {
-			var canvas;
-			var ctx;
-			var lastend = 0;
-			var myTotal = getTotal(myData);
-
-			ctx = canvas.getContext("2d");
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-			for (var i = 0; i < myData.length; i++) {
-				ctx.fillStyle = "#" + myColor[i];
-				ctx.beginPath();
-				ctx.moveTo(200,150);
-				ctx.arc(200,150,150,lastend,lastend+
-					(Math.PI*2*(myData[i]/myTotal)),false);
-				ctx.lineTo(200,150);
-				ctx.fill();
-				lastend += Math.PI*2*(myData[i]/myTotal);
-			}
-		}
-
-		var values = $.map( locations, function( val, i ) {
-				return val.frequency;
-			});
-		var colors = palette('sequential', locations.length);
-		var canvas = $("#canvas").get(0)
-		plotData(values, colors, canvas);
 	});
 });
